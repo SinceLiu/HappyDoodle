a6306c132d974ab1ea235712acae9f3a66d83366
kunchen <chenkun1983@163.com>
2019-06-28 10:20:20 +0800

    20190628

--- app/build/generated/source/buildConfig/release/com/readboy/Q/HappyDoodle/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/readboy/Q/HappyDoodle/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.readboy.Q.HappyDoodle";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 190626001;
-  public static final String VERSION_NAME = "1.3.190626001";
+  public static final int VERSION_CODE = 190627001;
+  public static final String VERSION_NAME = "1.3.190627001";
 }
